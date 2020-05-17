c5c994029841dbf9092ef3e9a25dde80119906e4
Denis Obrezkov <denisobrezkov@gmail.com>
2020-05-17 13:11:32 +0200

    Add more routines for a task

--- task.py
+++ task.py
@@ -2,6 +2,8 @@
 Class for a task abstraction.
 """
 
+from datetime import datetime
+
 class Task:
     """ This is a class representing task entity. """
 
@@ -15,7 +17,9 @@ class Task:
         :param activity_periods: list of tuples (start_time, end_time)
         """
         self.name = name
-        if activity_periods != None:
+        self.state = 'Stopped' # or Active, or Paused
+        self.current_start_time = None
+        if activity_periods is not None:
             self.activity_periods = activity_periods
         else:
             self.activity_periods = []
@@ -27,3 +31,22 @@ class Task:
     def add_activity_period(self, start_time, end_time):
         """ Add start and end times of a new activity periods into the list."""
         self.activity_periods.append((start_time, end_time))
+
+    def start_task(self):
+        """ Start countig time for the task. """
+        self.current_start_time = datetime.now()
+        self.state = 'Active'
+
+    def stop_task(self):
+        """ Stop counting time for the task. """
+        self.activity_periods.append((self.current_start_time, datetime.now()))
+        self.state = 'Stopped'
+
+    def pause_task(self):
+        """ Pause the counting time for the task. """
+        self.stop_task()
+        self.state('Stopped')
+
+    def unpause_task(self):
+        """ Unpause the paused task """
+        self.start_task()
